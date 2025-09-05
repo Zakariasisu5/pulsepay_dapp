d98d57a53a548be2bba287d4ff4360817a2b30fe
Zakariasisu5 <zakariasisu5@gmail.com>
2025-09-05 22:15:36 +0100

    commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -5,9 +5,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyCx89mhPNRmKbUNHdu60W6LNbhr1IZTEd8",
   authDomain: "pulsepay-5c7fe.firebaseapp.com",
   projectId: "pulsepay-5c7fe",
-  storageBucket: "pulsepay-5c7fe.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  storageBucket: "pulsepay-5c7fe.firebasestorage.app",
+  messagingSenderId: "806601337183",
+  appId: "1:806601337183:web:636ae0c6d2d6b8374b2eea",
+  measurementId: "G-HJTJTM3GT7"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
